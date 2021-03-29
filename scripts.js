7a7ea6acc92ff6f772a1d30842e02dbe7df2f8f0
blasesteban <blasesteban1@gmail.com>
2021-03-29 03:22:00 +0200

    s

--- scripts.js
+++ scripts.js
@@ -1,60 +1,62 @@
-$(function){
-let imagesData [{
-    photo: 'images/0ef9b9b86ac09b7f30de13a61ef80f0e.jpg'
-    title: 'baba1'
-    desc: 'kicsi baba deszép 1'
-}
+$(function(){
+let currentPhoto = 0;
+let imagesData = [{
+    photo: 'images/0ef9b9b86ac09b7f30de13a61ef80f0e.jpg',
+    title: 'baba1',
+    desc: 'kicsi baba deszép 1',
+},
 {
-    photo: 'images/104944-dove-bird-4k.jpg'
-    title: 'baba2'
-    desc: 'kicsi baba deszép 2'
-}
+    photo: 'images/104944-dove-bird-4k.jpg',
+    title: 'baba2',
+    desc: 'kicsi baba deszép 2',
+},
 {
-    photo: 'images/4505693-dove-birds-street-humor-pigeons.jpg'
-    title: 'baba3'
-    desc: 'kicsi baba deszép 3'
-}
-]
+    photo: 'images/4505693-dove-birds-street-humor-pigeons.jpg',
+    title: 'baba3',
+    desc: 'kicsi baba deszép 3',
+}];
+
 let loadPhoto = (photoNumber) => {
-    $('#photo').attr('src', imagesData[photoNumber].photo)
-    $('#title').text('src', imagesData[photoNumber].title)
-    $('#desc').text('src', imagesData[photoNumber].desc)
+    $('#photo').attr('src', imagesData[photoNumber].photo);
+    $('#title').text(imagesData[photoNumber].title);
+    $('#desc').text(imagesData[photoNumber].desc);
   }
-let currentPhoto = 0
-loadPhoto(currentPhoto)
+loadPhoto(currentPhoto);
 
 $('#prev').click(() => {
   if (currentPhoto>0){currentPhoto--}
-  else {currentPhoto==imagesdata.length}
+  else {currentPhoto=imagesData.length-1}
   loadPhoto(currentPhoto);
-})
+});
 $('#next').click(() => {
-  if (currentPhoto<imagesdata.length){currentPhoto++}
-  else {currentPhoto==0}
-})
+  if (currentPhoto<imagesData.length-1){currentPhoto++}
+  else {currentPhoto=0}
+  loadPhoto(currentPhoto);
+});
+
 imagesData.forEach((item, index) => {
-  $('#thumbnails').append(`<div class='mini' data-number='${index}'>
-  <img src='${item.photo}' data-number='${index}'>
-  </div>`)
-$('.mini').click((event) => {
-    let photoNumber=parseInt($(event.target).attr('data-number'))
-    loadPhoto(photoNumber)
-  })
-})
+  $('.thumbnails').append(`<div class='thumbnail' data-number='${index}'>
+  <img src='${item.photo}' data-number='${index}' alt=''>
+  </div>`);
+  $('.thumbnail').click((event) => {
+    let photoNumber=parseInt($(event.target).attr('data-number'));
+    loadPhoto(photoNumber);
+  });
+});
 imagesData.forEach((item, index) => {
-  $('#thumbnails').append(`<div class='minititle' data-number='${index}'>
+  $('.thumbnail').append(`<div class='minititle' data-number='${index}'>
   ${item.title}
   </div>`)
-$('.mini').mouseover((event) => {
-    let photoNumber=parseInt($(event.target).attr('data-number'))
-    let offst= ${`.mini[data-number='${photoNumber}']`}.offset().top = 10
-    let offsl= ${`.mini[data-number='${photoNumber}']`}.offset().left = 10
-    $(`.title[data-number='${photoNumber}']`).css('position','absolute')
-    $(`.title[data-number='${photoNumber}']`).show().offset({top:offst, left:offsl})
-  })
-  $('.mini').mouseout(function(event){
-    let photoNumber=parseInt($(event.target).attr('data-number'))
-    $(`.title[data-number='${photoNumber}']`).hide
-  })
+});
+$('.thumbnail').mouseover((event) => {
+    let photoNumber=parseInt($(event.target).attr('data-number'));
+    let offst= $(`.thumbnail[data-number='${photoNumber}']`).offset().top = 10
+    let offsl= $(`.thumbnail[data-number='${photoNumber}']`).offset().left = 10
+    $(`.minititle[data-number='${photoNumber}']`).css('position','absolute');
+    $(`.minititle[data-number='${photoNumber}']`).show().offset({top:offst, left:offsl});
+  });
+  $('.thumbnail').mouseout((event) => {
+    let photoNumber=parseInt($(event.target).attr('data-number'));
+    $(`.minititle[data-number='${photoNumber}']`).hide
+  });
 })
-}
